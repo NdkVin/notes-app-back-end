f781aa21479c321e04f477eaa2476395230b66ad
Andika <andika.kavin@gmail.com>
2021-11-24 14:08:38 +0700

    eslint fix

--- src/exceptions/ClientError.js
+++ src/exceptions/ClientError.js
@@ -1,9 +1,12 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-expressions */
+
 class ClientError extends Error {
-    constructor(message, statusCode = 400) {
-        super(message);
-        this.statusCode = statusCode,
-        this.name = 'CluentError'
-    }
+  constructor(message, statusCode = 400) {
+    super(message);
+    this.statusCode = statusCode;
+    this.name = 'CluentError';
+  }
 }
 
 module.exports = ClientError;
--- src/validator/notes/index.js
+++ src/validator/notes/index.js
@@ -1,14 +1,14 @@
+/* eslint-disable linebreak-style */
 const InvariantError = require('../../exceptions/InvariantError');
 const { NotePayloadSchema } = require('./schema');
 
 const NotesValidator = {
-    validateNotePayload : (payload) => {
-        const validateResult = NotePayloadSchema.validate(payload);
-
-        if(validateResult.error) {
-            throw new InvariantError(validateResult.error.message);
-        }
-    },
-}
+  validateNotePayload: (payload) => {
+    const validateResult = NotePayloadSchema.validate(payload);
+    if (validateResult.error) {
+      throw new InvariantError(validateResult.error.message);
+    }
+  },
+};
 
 module.exports = NotesValidator;
